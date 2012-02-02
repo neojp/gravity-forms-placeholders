e1a8e5a18398e71237405c0c4db6f220e63e74c6
Joan Piedra <joan@joanpiedra.com>
2012-02-02 11:21:09 -0500

    Placeholder plugin for Gravity Forms

---
+++ gravityforms-placeholders.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Gravity Forms - Placeholders add-on
+Plugin URI: http://joanpiedra.com/wordpress/gravity-forms-placeholders/
+Description: Adds HTML5 placeholder support to Gravity Forms' fields with a javascript fallback. Javascript & jQuery are required.
+Version: 1.0
+Author: Joan Piedra
+Author URI: http://joanpiedra.com
+
+Instructions:
+Just add a "gplaceholder" CSS classname to the required fields.
+
+*/
+
+if (is_admin())
+	return;
+
+wp_enqueue_script("_gf_placeholders", plugins_url(null, __FILE__) . "/gf.placeholders.js.php", array("jquery"), "1.0");
