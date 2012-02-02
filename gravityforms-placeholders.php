fa0089394e736cdef658df94ec134408529b2c51
Joan Piedra <joan@joanpiedra.com>
2012-02-02 11:53:36 -0500

    Do not enqueue the js file on either the admin or login pages

--- gravityforms-placeholders.php
+++ gravityforms-placeholders.php
@@ -12,7 +12,7 @@ Just add a "gplaceholder" CSS classname to the required fields.
 
 */
 
-if (is_admin())
+if (is_admin() || (iseet($GLOBALS['pagenow']) && $GLOBALS['pagenow'] == 'wp-login.php'))
 	return;
 
 wp_enqueue_script("_gf_placeholders", plugins_url(null, __FILE__) . "/gf.placeholders.js.php", array("jquery"), "1.0");
